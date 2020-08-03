3b86aea5ab0b20d9d8f7cf1ce840e6cfc50dc033
qqYe <53002872+qqYe@users.noreply.github.com>
2020-08-03 11:53:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "ibm_is_floating_ip" "floatingip3" {
 }
 
 resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
-  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
+  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2", "ibm_is_floating_ip.floatingip3"]
   group     = "${ibm_is_vpc.vpc1.default_security_group}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
